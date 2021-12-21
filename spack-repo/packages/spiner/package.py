a5defbada3cf71f492498c11c315bb74f47e54f7
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2021-12-20 18:26:53 -0700

    try adding kokkos and cuda deps

--- spack-repo/packages/spiner/package.py
+++ spack-repo/packages/spiner/package.py
@@ -2,7 +2,7 @@
 
 from spack import *
 
-class Spiner(CMakePackage):
+class Spiner(CMakePackage, CudaPackage):
     """Spiner"""
 
     homepage    = "https://github.com/lanl/spiner"
@@ -11,12 +11,24 @@ class Spiner(CMakePackage):
 
     version("main", branch="main")
 
+    variant("kokkos", default=False, description="Enable kokkos")
+    variant("kokkos", default=False, description="Enable kokkos")
+
     variant("python", default=False, description="Python, Numpy & Matplotlib Support")
     variant("doc", default=False, description="Sphinx Documentation Support")
     variant("format", default=False, description="Clang-Format Support")
 
     depends_on("cmake@3.12:")
     depends_on("catch2@2.13.4:2.13.6")
+
+    for _flag in list(CudaPackage.cuda_arch_values):
+        depends_on("kokkos@3.3: cuda_arch=" +_flag, when="+cuda+kokkos cuda_arch=" + _flag)
+    
+    depends_on("kokkos@3.3:~shared+wrapper+cuda_lambda+cuda_relocatable_device_code", when="+cuda+kokkos")
+    depends_on("kokkos-nvcc-wrapper", when="+cuda+kokkos")
+
+    for _flag in ("~cuda", "+cuda", "~openmp", "+openmp"):
+        depends_on("kokkos@3.3:" + _flag, when="+kokkos" + _flag)
 
     depends_on("python", when="+python")
     depends_on("py-numpy", when="+python")
@@ -27,6 +39,9 @@ class Spiner(CMakePackage):
     depends_on("py-sphinx-multiversion", when="+doc")
 
     depends_on("llvm@12.0.0+clang", when="+format")
+
+    conflicts("+cuda", when="~kokkos")
+    conflicts("cuda_arch=none", when="+cuda", msg="CUDA architecture is required")
 
     def cmake_args(self):
         args = [
