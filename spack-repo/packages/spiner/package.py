82a056001f40b5d2fddc31a4ceb68137737b6e46
Karen C. Tsai <ktsai@lanl.gov>
2021-12-21 11:52:31 -0700

    Add Gitlab CI and Sphinx Documentation template (#16)
    * initial files
    
    * Update sphinx-howto.rst
    
    * spiner-deps
    
    * fix spiner-deps name
    
    * change to spiner spackage iinstead of -deps
    
    * update clang-format module and triggered pipelines do not deploy pages
    
    * update spack env command
    
    * create build folder and go in it
    
    * fix typo
    
    * cmake install prefix
    
    * add format dep
    
    * --show-log-on-error --no-checksum --yes-to-all
    
    * add python deps
    
    * build the overall spackage only in build/test jobs
    
    * do not need to set permission during CI Spack
    
    * minor improvement
    
    * cmake now checks if python is available
    
    * add cmake options
    
    * trying double []
    
    * try adding nvcc_wrapper
    
    * try adding kokkos and cuda deps
    
    * kokkos already have kokkos-nvcc-wrapper under wrapper variant
    
    * @3.2.00
    
    * module load cuda
    
    * +cuda and arch 70
    
    * cuda_arch
    
    * conditional for kokkos and cuda deps
    
    * fix typo
    
    Co-authored-by: Jonah Maxwell Miller <jonahm@lanl.gov>

---
+++ spack-repo/packages/spiner/package.py
@@ -0,0 +1,46 @@
+# Spackage for Spiner
+
+from spack import *
+
+class Spiner(CMakePackage, CudaPackage):
+    """Spiner"""
+
+    homepage    = "https://github.com/lanl/spiner"
+    url         = "https://github.com/lanl/spiner/archive/refs/heads/main.zip"
+    git         = "git@github.com:lanl/spiner.git"
+
+    version("main", branch="main")
+
+    variant("kokkos", default=False, description="Enable kokkos")
+
+    variant("python", default=False, description="Python, Numpy & Matplotlib Support")
+    variant("doc", default=False, description="Sphinx Documentation Support")
+    variant("format", default=False, description="Clang-Format Support")
+
+    depends_on("cmake@3.12:")
+    depends_on("catch2@2.13.4:2.13.6")
+
+    for _flag in list(CudaPackage.cuda_arch_values):
+        depends_on("kokkos@3.2.00 cuda_arch=" + _flag, when="+cuda+kokkos cuda_arch=" + _flag)
+    for _flag in ("~cuda", "+cuda", "~openmp", "+openmp"):
+        depends_on("kokkos@3.2.00" + _flag, when="+kokkos" + _flag)
+    depends_on("kokkos@3.2.00~shared+wrapper+cuda_lambda+cuda_relocatable_device_code", when="+cuda+kokkos")
+
+    depends_on("python", when="+python")
+    depends_on("py-numpy", when="+python")
+    depends_on("py-matplotlib", when="+python")
+
+    depends_on("py-sphinx", when="+doc")
+    depends_on("py-sphinx-rtd-theme@0.4.3", when="+doc")
+    depends_on("py-sphinx-multiversion", when="+doc")
+
+    depends_on("llvm@12.0.0+clang", when="+format")
+
+    conflicts("+cuda", when="~kokkos")
+    conflicts("cuda_arch=none", when="+cuda", msg="CUDA architecture is required")
+
+    def cmake_args(self):
+        args = [
+            "-DBUILD_TESTING={0}".format("ON" if self.run_tests else "OFF"),
+        ]
+        return args
