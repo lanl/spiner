6e3fd1a77025c438f4e379860c0bb4d27caf0ee6
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2021-12-20 20:46:32 -0700

    +cuda and arch 70

--- spack-repo/packages/spiner/package.py
+++ spack-repo/packages/spiner/package.py
@@ -22,11 +22,9 @@ class Spiner(CMakePackage, CudaPackage):
 
     for _flag in list(CudaPackage.cuda_arch_values):
         depends_on("kokkos@3.2.00 cuda_arch=" +_flag, when="+cuda+kokkos cuda_arch=" + _flag)
-
     for _flag in ("~cuda", "+cuda", "~openmp", "+openmp"):
         depends_on("kokkos@3.2.00" + _flag, when="+kokkos" + _flag)
-
-    depends_on("kokkos@3.2.00~shared+wrapper+cuda_lambda+cuda_relocatable_device_code", when="+cuda+kokkos")
+    depends_on("kokkos@3.2.00~shared+wrapper+cuda+cuda_lambda+cuda_relocatable_device_code cuda_arch=70", when="+cuda+kokkos")
 
     depends_on("python", when="+python")
     depends_on("py-numpy", when="+python")
