dae651ae6186494b931b08a19aaac217e2f5c587
Richard Berger <rberger@lanl.gov>
2022-12-01 15:25:45 -0700

    Update Catch2 depenency to 2.13.7

--- spack-repo/packages/spiner/package.py
+++ spack-repo/packages/spiner/package.py
@@ -34,7 +34,7 @@ class Spiner(CMakePackage, CudaPackage):
     variant("format", default=False, description="Clang-Format Support")
 
     depends_on("cmake@3.12:")
-    depends_on("catch2@2.13.4:2.13.6")
+    depends_on("catch2@2.13.7:2.13.10", type="test")
     depends_on("ports-of-call@1.2.0:")
 
     # Currently the raw cuda backend of ports-of-call is not supported.
