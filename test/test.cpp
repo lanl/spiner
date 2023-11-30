90d50448e9306863c58d2406fade0d9681ca5abc
Jonah Miller <jonahm@lanl.gov>
2023-11-30 11:36:09 -0700

    Revert "update catch2 v2 to catch2 v3"
    This reverts commit 132b17c4094549ec9d72bf4843f5a2a3ec6f6e34.

--- test/test.cpp
+++ test/test.cpp
@@ -29,8 +29,8 @@
 #include "hdf5_hl.h"
 #endif
 
-#include <catch2/catch_session.hpp>
-#include <catch2/catch_test_macros.hpp>
+#define CATCH_CONFIG_RUNNER
+#include "catch2/catch.hpp"
 
 using DataBox = Spiner::DataBox<Real>;
 using Spiner::IndexType;
