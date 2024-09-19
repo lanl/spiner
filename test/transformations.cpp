fa2dd5b8925324dfa63cbbd305eb3a087b225892
Brendan K. Krueger <bkkrueger@lanl.gov>
2024-09-19 14:35:26 -0600

    Add GPU testing for transformations.

--- test/transformations.cpp
+++ test/transformations.cpp
@@ -23,21 +23,50 @@ using Catch::Matchers::WithinAbs;
 using Catch::Matchers::WithinRel;
 using Catch::Matchers::WithinULP;
 
+namespace {
+    PORTABLE_FUNCTION double relerr(const double z, const double zz) {
+        if (z == 0) {
+            return std::abs(zz - z);
+        } else {
+            return std::abs(double(1) - zz/z);
+        }
+    }
+};
+
 TEST_CASE("transformation: linear", "") {
     // This one is almost too simple to meaningfully test, but we can at least
     // ensure that it compiles and does the trivially-obvious things.
     using Transform = Spiner::TransformLinear;
+
+    // Test on CPU
     const double x0 = 3.14159;
     CHECK(Transform::forward(x0) == x0);
     CHECK(Transform::reverse(x0) == x0);
     const float x1 = 2.71828;
     CHECK(Transform::forward(x1) == x1);
     CHECK(Transform::reverse(x1) == x1);
-    // TODO: Test on GPU
+
+    // Test on GPU (or CPU if no GPU available)
+    const int num_threads = 10;
+    double accum = 0;
+    portableReduce(
+        "parallel_reduce", 0, num_threads,
+        PORTABLE_LAMBDA(const int n, double &reduce)
+        {
+            const double x = static_cast<double>(n);
+            const double y = x;
+            const double yy = Transform::forward(x);
+            const double xx = Transform::reverse(y);
+            reduce += 0.5 * (relerr(x, xx) + relerr(y, yy));
+        },
+        accum);
+    CHECK(accum / num_threads == 0);
 }
 
 TEST_CASE("transformation: logarithmic", "") {
     using Transform = Spiner::TransformLogarithmic;
+
+    // Test on CPU
     auto matcher = [](const double d) {
         //return WithinULP(d, 500);
         return WithinRel(d, 6.0e-12);
@@ -55,6 +84,22 @@ TEST_CASE("transformation: logarithmic", "") {
     // Special value
     CHECK(std::isfinite(Transform::forward(0)));
     CHECK(Transform::reverse(Transform::forward(0)) == 0);
-    // TODO: Test on GPU
+
+    // Test on GPU (or CPU if no GPU available)
+    const int num_threads = 101;
+    double accum = 0;
+    portableReduce(
+        "parallel_reduce", 0, num_threads,
+        PORTABLE_LAMBDA(const int n, double &reduce)
+        {
+            const int p = n - num_threads / 2;
+            const double x = std::pow(double(10.0), p);
+            const double y = std::log(x);
+            const double yy = Transform::forward(x);
+            const double xx = Transform::reverse(y);
+            reduce += 0.5 * (relerr(x, xx) + relerr(y, yy));
+        },
+        accum);
+    CHECK(accum / num_threads <= 6.0e-12);
 }
 
